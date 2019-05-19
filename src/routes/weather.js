0e5ccc929ad972f77833c9e1ee3304237b295d84
BNEJesse <491974150@qq.com>
2019-05-19 23:28:55 +1000

    add dotenv

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const APPID = process.env.APPID;
 
 router.get('/:cc/:city', (req, res) => {
   //   res.send('weather');
   const { cc, city } = req.params;
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=3114e860a38cebb221575bb046d93fce`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=${APPID}`
     )
     .then(response => {
       res.send(response.data);
