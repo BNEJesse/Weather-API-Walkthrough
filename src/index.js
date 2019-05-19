b21ad9f85430e7bee6131b1c860bd5980dd78ad6
BNEJesse <491974150@qq.com>
2019-05-20 01:02:52 +1000

    handle 404

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const logger = require('./utils/logger');
-
+const notFoundHandler = require('./middlewares/notFound');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -17,4 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(routes);
+app.use(notFoundHandler);
+
 app.listen(PORT, () => logger.info(`app listen on port ${PORT}`));
